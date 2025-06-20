db5fe2d53232b29be38a76bdb9c6e8ffa3ae3d6f
Zara <zarazwak188@gmail.com>
2025-06-19 22:19:16 -0700

    worked on browse job

---
+++ src/components/BrowseJob/Main.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="bg-white px-36">
+      <main>
+        <h1 className="text-black text-3xl font-bold mb-8 mt-8">Browse Job</h1>
+        <p className="text-background font-semibold">Companies By letter</p>
+        <ul className="text-blue-800 grid gap-3 m-3 grid-flow-col"> 
+          <li>
+            <a href="#">A</a>
+          </li>
+          <li>
+            <a href="#">B</a>
+          </li>
+          <li>
+            <a href="#">C</a>
+          </li>
+          <li>
+            <a href="#">D</a>
+          </li>
+          <li>
+            <a href="#">E</a>
+          </li>
+          <li>
+            <a href="#">F</a>
+          </li>
+          <li>
+            <a href="#">G</a>
+          </li>
+          <li>
+            <a href="#">H</a>
+          </li>
+          <li>
+            <a href="#">I</a>
+          </li>
+          <li>
+            <a href="#">J</a>
+          </li>
+          <li>
+            <a href="#">K</a>
+          </li>
+          <li>
+            <a href="#">L</a>
+          </li>
+          <li>
+            <a href="#">M</a>
+          </li>
+          <li>
+            <a href="#">N</a>
+          </li>
+          <li>
+            <a href="#">O</a>
+          </li>
+          <li>
+            <a href="#">P</a>
+          </li>
+          <li>
+            <a href="#">Q</a>
+          </li>
+          <li>
+            <a href="#">R</a>
+          </li>
+          <li>
+            <a href="#">S</a>
+          </li>
+          <li>
+            <a href="#">T</a>
+          </li>
+          <li>
+            <a href="#">U</a>
+          </li>
+          <li>
+            <a href="#">V</a>
+          </li>
+          <li>
+            <a href="#">W</a>
+          </li>
+          <li>
+            <a href="#">X</a>
+          </li>
+          <li>
+            <a href="#">Y</a>
+          </li>
+          <li>
+            <a href="#">Z</a>
+          </li>
+
+          <li>
+            <a href="#">0-9</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
---
+++ src/pages/browse-job.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '@/components/BrowseJob/Main'
+export default function BrowseJob() {
+  return (
+    <div className='w-full min-h-screen bg-gray-200'>
+   <Main />
+   </div>
+  )
+}
