31359af73660e0c887d58de1f7e931f29474def7
Zara <zarazwak188@gmail.com>
2025-06-24 22:12:08 -0700

    worked with js of browse page

--- src/components/BrowseJob/Main.jsx
+++ src/components/BrowseJob/Main.jsx
@@ -1,340 +1,132 @@
 import Link from "next/link";
 import React from "react";
-
+const Alphabet = [
+  "A",
+  "B",
+  "D",
+  "C",
+  "E",
+  "G",
+  "F",
+  "H",
+  "I",
+  "J",
+  "L",
+  "K",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const JobByLocation = [
+  "British Overseas Territoris",
+  "Scotland",
+  "England",
+  "Wales",
+  "Northren Ireland",
+];
+const JobByCategory = [
+  "Accounting",
+  "Administritive Assistance",
+  "Agriculture and forestry",
+  "Archticture",
+  "Arts and entertenement",
+  "Aviation",
+  "Banking and fineces",
+  "Beauty and wellness",
+  "Chimichal Engineering",
+  "ChildCare",
+  "Civil Engineering",
+  "Customer Service",
+  "Dental",
+  "Driving",
+  "Education & Instruction",
+  "Electrical Engineering",
+  "Hospitality & Tourism",
+  "Human Resources",
+  "Industrial Engineering",
+  "Information Design & Documentation",
+  "Installation & Maintenance",
+  "Insurance",
+  "Legal",
+  "Loading and Stocking",
+  "Logistic Support",
+  "Management",
+  "Marketing",
+  "Mathematics",
+  "Military",
+  "Mining",
+  "Nursing",
+  "Personal Care & Home Health",
+  "Pharmacy",
+  "Physicians & Surgeons",
+  "Production & Manufacturing",
+  "Project Management",
+  "Real Estate",
+  "Retail",
+  "Sales",
+  "Scientific Research & Development",
+  "Security & Public Safety",
+  "Social Science",
+  "Software Development",
+  "Sports",
+  "Therapy",
+  "Veternairy",
+];
 export default function Main() {
   return (
-    <div className="bg-white px-72">
+    <div className="bg-white container mx-auto px-4 flex justify-center">
       <main>
         <h1 className="text-black text-3xl font-bold py-8">Browse Job</h1>
         <p className="text-background font-semibold">Companies By letter</p>
         <ul className="text-[#2557A7] text-[0.90rem] pb-12 flex flex-wrap py-3 justify-start items-center gap-7 ">
-          <li >
-            <a href="#">A</a>
-          </li>
-          <li>
-            <a href="#">B</a>
-          </li>
-          <li>
-            <a href="#">C</a>
-          </li>
-          <li>
-            <a href="#">D</a>
-          </li>
-          <li>
-            <a href="#">E</a>
-          </li>
-          <li>
-            <a href="#">F</a>
-          </li>
-          <li>
-            <a href="#">G</a>
-          </li>
-          <li>
-            <a href="#">H</a>
-          </li>
-          <li>
-            <a href="#">I</a>
-          </li>
-          <li>
-            <a href="#">J</a>
-          </li>
-          <li>
-            <a href="#">K</a>
-          </li>
-          <li>
-            <a href="#">L</a>
-          </li>
-          <li>
-            <a href="#">M</a>
-          </li>
-          <li>
-            <a href="#">N</a>
-          </li>
-          <li>
-            <a href="#">O</a>
-          </li>
-          <li>
-            <a href="#">P</a>
-          </li>
-          <li>
-            <a href="#">Q</a>
-          </li>
-          <li>
-            <a href="#">R</a>
-          </li>
-          <li>
-            <a href="#">S</a>
-          </li>
-          <li>
-            <a href="#">T</a>
-          </li>
-          <li>
-            <a href="#">U</a>
-          </li>
-          <li>
-            <a href="#">V</a>
-          </li>
-          <li>
-            <a href="#">W</a>
-          </li>
-          <li>
-            <a href="#">X</a>
-          </li>
-          <li>
-            <a href="#">Y</a>
-          </li>
-          <li>
-            <a href="#">Z</a>
-          </li>
-
-          <li>
-            <a href="#">0-9</a>
-          </li>
+          {Alphabet.sort().map((value, index) => {
+            return (
+              <li key={index}>
+                <Link href="#">{value}</Link>
+              </li>
+            );
+          })}
         </ul>
         <p className="text-black font-semibold">Jobs By Title</p>
         <ul className="text-[#2557A7] text-[0.90rem] pb-12 flex flex-wrap py-3 justify-start items-center gap-7 ">
-          <li>
-            <a href="#">A</a>
-          </li>
-          <li>
-            <a href="#">B</a>
-          </li>
-          <li>
-            <a href="#">C</a>
-          </li>
-          <li>
-            <a href="#">D</a>
-          </li>
-          <li>
-            <a href="#">E</a>
-          </li>
-          <li>
-            <a href="#">F</a>
-          </li>
-          <li>
-            <a href="#">G</a>
-          </li>
-          <li>
-            <a href="#">H</a>
-          </li>
-          <li>
-            <a href="#">I</a>
-          </li>
-          <li>
-            <a href="#">J</a>
-          </li>
-          <li>
-            <a href="#">K</a>
-          </li>
-          <li>
-            <a href="#">L</a>
-          </li>
-          <li>
-            <a href="#">M</a>
-          </li>
-          <li>
-            <a href="#">N</a>
-          </li>
-          <li>
-            <a href="#">O</a>
-          </li>
-          <li>
-            <a href="#">P</a>
-          </li>
-          <li>
-            <a href="#">Q</a>
-          </li>
-          <li>
-            <a href="#">R</a>
-          </li>
-          <li>
-            <a href="#">S</a>
-          </li>
-          <li>
-            <a href="#">T</a>
-          </li>
-          <li>
-            <a href="#">U</a>
-          </li>
-          <li>
-            <a href="#">V</a>
-          </li>
-          <li>
-            <a href="#">W</a>
-          </li>
-          <li>
-            <a href="#">X</a>
-          </li>
-          <li>
-            <a href="#">Y</a>
-          </li>
-          <li>
-            <a href="#">Z</a>
-          </li>
-
-          <li>
-            <a href="#">0-9</a>
-          </li>
+          {Alphabet.sort().map((value, index) => {
+            return (
+              <li key={index}>
+                <Link href="#">{value}</Link>
+              </li>
+            );
+          })}
         </ul>
         <h2 className="text-black font-semibold py-3 pb-4">Jobs By Location</h2>
         <ul className="grid grid-cols-3 text-[#2557A7] pb-12">
-          <li className="hover:underline">
-            <Link href="#">British Overseas Territoris</Link>
-          </li>
-
-          <li className="hover:underline">
-            <Link href="#">England</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="#">Northren Ireland</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="#">Scotland</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="#">Wales</Link>
-          </li>
+          {JobByLocation.map((value, index) => {
+            return (
+              <li className="hover:underline" key={index}>
+                <Link href="#">{value}</Link>
+              </li>
+            );
+          })}
         </ul>
-        <h2 className="text-black font-semibold pb-4">Jobs By Location</h2>
+        <h2 className="text-black font-semibold pb-4">Jobs By Category</h2>
         <ul className="grid grid-cols-3 text-blue-800 font-[0.90rem]">
-          <li className="pb-4 hover:underline">
-            <Link href="#">Accounting</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Administritive Assistance</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Agriculture and forestry</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Archticture</Link>
-          </li>
-          <li className=" pb- hover:underline4">
-            <Link href="#">Arts and entertenement</Link>
-          </li>
-          <li className=" pb- hover:underline4">
-            <Link href="#">Aviation</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Banking and fineces</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Beauty and wellness</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Chimichal Engineering </Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">ChildCare</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Civil Engineering</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Customer Service</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Dental</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Driving</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Education & Instruction</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Electrical Engineering</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Hospitality & Tourism</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Human Resources</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Industrial Engineering</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Information Design & Documentation</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Installation & Maintenance</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Insurance</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Legal</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Loading and Stocking</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Logistic Support</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Management</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Marketing</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Mathematics</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Military</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Mining</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Nursing</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Personal Care & Home Health</Link>
-          </li>
-          <li className="pb-4 hover:underline">
-            <Link href="#">Pharmacy</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Physicians & Surgeons</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Production & Manufacturing</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Project Management</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Real Estate</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Retail</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Sales</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Scientific Research & Development</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Security & Public Safety</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Social Science</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Software Development</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Sports</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Therapy</Link>
-          </li>{" "}
-          <li className="pb-4 hover:underline">
-            <Link href="#">Veternairy</Link>
-          </li>
+          {JobByCategory.map((value, index) => {
+            return (
+              <li className="hover:underline pb-4" key={index}>
+                <Link href="#">{value}</Link>
+              </li>
+            );
+          })}
         </ul>
       </main>
     </div>
