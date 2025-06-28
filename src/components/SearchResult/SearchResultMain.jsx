c83dffe3b67dfe74de0a6f9cde91e81c7bf17a66
Nafisa Ebrahimi <nafisai2750@gmail.com>
2025-06-28 06:25:22 -0700

    created jobdata in array objec

--- src/components/SearchResult/SearchResultMain.jsx
+++ src/components/SearchResult/SearchResultMain.jsx
@@ -2,33 +2,98 @@ import React, { useState } from "react";
 import { FaArrowRight, FaSearch, FaMapMarkerAlt } from "react-icons/fa";
 import Link from "next/link";
 
-function JobsPost() {
+const jobdata=[
+    {
+        title:'Trending searches',
+        items:[
+            "castle donington de74",
+            "sponsorship available",
+            "abbotts ann sp11",
+            "maghull l31",
+            "kettering kettering",
+            "whoberley cv1"
+        ],
+        title:"Trending jobs",
+        items:[
+            "content creator remote",
+            "retail london",
+                "retail london",
+              "fully remote remote",
+              "part time england",
+              "finance remote",
+              "full time bristol",
+              "administration remote",
+              "project manager remote",
+              "communications remote",
+              "part time remote",
+              "graphic design remote",
+
+        ],
+        tilte:"Popular cities",
+        items:[
+             "slough",
+              "london",
+              "carlisle",
+              "stafford",
+              "wrexham",
+              "birmingham",
+              "halifax telford",
+              "derby",
+              "cardiff",
+              "glasgow",
+              "tamworth",
+               "chester",
+              "newport",
+              " bournemouth",
+              " exeter",
+              "blackpool",
+              " croydon",
+
+        ],
+        tilte:"Popular companies",
+        items:[
+           "pets at home",
+              "holland %26",
+              "h%26m",
+              "salesforce",
+              "whsmith",
+              "currys",
+              "iss facility services",
+              "the range",
+              "barrett",
+              " asda",
+              " b%26m",
+
+        ]
+    }
+]
+
+function JobPosts() {
   return (
     <div className="bg-white w-full">
-      <div className="bg-white flex justify-center gap-2 p-2 text-2xl font-bold text-blue-800">
+      <div className="bg-white flex justify-center gap-2 p-2 text-2xl font-bold text-[#2557a7]">
         <Link href="/#" className="underline">
           Upload your CV
         </Link>
-        <p className="font-semibold text-xl text-black">
+        <p className="font-noraml text-xl text-black">
           - It only takes a few seconds
         </p>
       </div>
-      <div className="bg-white flex justify-center text-2xl  mt-3 underline text-blue-800 font-bold">
+      <div className="bg-white flex justify-center text-2xl  mt-3 underline text-[#2557a7] font-bold">
         <Link href="/#">Post a job on Indeed</Link>
       </div>
     </div>
   );
 }
 
-function JobsLists() {
+const  JobLists =() => {
   return (
     <div className="w-full text-2xl text-black flex flex-col justify-center items-center">
-      <Link href="#">
-        <button type="button" className="bg-amber-500 w-full h-auto mt-6">
-          What's trending on Indeed
+        <button type="button" className="bg-white  w-fit h-auto mt-6">
+        <Link href="#" className="text-xl ">  What's trending on Indeed</Link>
         </button>
-      </Link>
       <div className="bg-white w-full text-xl font-bold text-black mt-8 flex justify-start pl-5 gap-4">
+        
         <div className="w-full">
           <h2>Trending searches</h2>
           <ul className="bg-white  text-blue-700 underline mt-5">
@@ -136,13 +201,13 @@ export default function SearchResultMain() {
 
         {/* Find Jobs Button */}
         <Link href="#">
-          <button className="bg-blue-700 text-white p-4 rounded-xl hover:bg-green-800 w-32 ml-4 font-bold">
+          <button className="bg-[#2557a7] text-white p-4 rounded-xl hover:bg-green-800 w-32 ml-4 font-bold">
             Find Jobs
           </button>
         </Link>
       </div>
       {/* background section */}
-      <div className="w-full bg-[#EEF1FF] pt-5 flex justify-center">
+      <div className="w-full bg-[#e8e2fe] pt-5 flex justify-center">
         <img
           src="/images/img.jpg"
           className="w-auto  h-[300px] object-contain"
@@ -160,14 +225,14 @@ export default function SearchResultMain() {
         <div className="flex justify-center items-center p-6">
           <Link
             href="/#"
-            className="bg-blue-700 hover:bg-blue-800 text-white font-semibold px-6 py-3 rounded-lg border border-white flex items-center gap-2"
+            className="bg-[#2557a7] text-white font-semibold px-6 py-3 rounded-lg border border-white flex items-center gap-2"
           >
             Get started
             <FaArrowRight />
           </Link>
         </div>
-        <JobsPost />
-        <JobsLists />
+        <JobPosts />
+        <JobLists />
       </div>
     </div>
   );
