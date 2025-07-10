bed24012128cbe909df14edb799b484a02e59d2b
Nafisa Ebrahimi <nafisai2750@gmail.com>
2025-07-09 22:49:22 -0700

    add footer component and footer page

---
+++ src/components/Footer/MainFooter.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function MainFooter() {
+    const datafOOterLists=[
+        {
+            items:[
+                'Hiring Lab',
+                'Career advice',
+                'Browse jobs',
+                'Browse companies',
+                'Salaries',
+                'Indeed Events',
+                'Work at Indeed',
+                'Countries',
+                'About',
+                'Help',
+                'ESG at Indeed',
+                'Post a job'
+
+            ],
+        },
+        {
+            items:[
+                'Anti-Slavery statement',
+                'Accessibility at Indeed',
+                'Privacy Centre and Ad Choices',
+                'Terms'
+            ],
+        }
+    ]
+
+  return (
+    <div className='bg-amber-200 w-full'>
+        <div>
+            <h2>footer</h2>
+        </div>
+
+
+
+    </div>
+  )
+}
---
+++ src/pages/footer/footer.js
@@ -0,0 +1,10 @@
+import MainFooter from '@/components/Footer/MainFooter';
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <MainFooter/>
+    </div>
+  )
+}
