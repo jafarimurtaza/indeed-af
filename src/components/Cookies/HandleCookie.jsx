c6d8d99d990d1cceaa25e7b3eeb91015e1c95aca
Zara <zarazwak188@gmail.com>
2025-07-20 22:08:24 -0700

    worked on accept and reject cookie buttons

--- src/components/Cookies/HandleCookie.jsx
+++ src/components/Cookies/HandleCookie.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
+const buttonStyle =
+  "bg-blue-900 w-full md:h-14 h-12 rounded-[0.3rem] p-2 hover:cursor-pointer hover:bg-blue-800 text-white font-bold";
 export default function HandleCookie() {
-  const buttonStyle =
-    "bg-blue-900 w-full md:h-14 h-12 rounded-[0.3rem] p-2 hover:cursor-pointer hover:bg-blue-800 text-white font-bold";
+  const [reject, setReject] = useState(false);
+  const [accept, setAccept] = useState(false);
+  const HandleAccept = () => {
+    setAccept(true);
+    localStorage.setItem("IsCookieAccepted", "Accepted");
+  };
+  const HandleReject = () => {
+    setReject(true);
+    localStorage.setItem("IsRejectd", "Rejected");
+  };
+  if (typeof window === !"undefiend") {
+    const isCookieRejected = localStorage.getItem("IsRejected");
+  }
+  if (reject || accept) {
+    return null;
+  }
+
   return (
     <div className="bg-indigo-100 w-full h-auto gap-3 flex lg:flex-row flex-col px-6 py-4">
       <p className="text-black text-[1.3rem]">
@@ -14,8 +30,12 @@ export default function HandleCookie() {
       </p>
       <div className="flex flex-col md:flex-row w-full items-center gap-5">
         <button className={buttonStyle}>Cookie settings</button>
-        <button className={buttonStyle}>Reject All</button>
-        <button className={buttonStyle}>Accept All Cookie</button>
+        <button className={buttonStyle} onClick={HandleReject}>
+          Reject All
+        </button>
+        <button className={buttonStyle} onClick={HandleAccept}>
+          Accept All Cookie
+        </button>
       </div>
     </div>
   );
