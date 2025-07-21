d15d503fee0010346365972982e5b8c911657535
nooriaAhsani216 <nooriaahsani216@gmail.com>
2025-07-21 08:37:55 +0430

    worked on Maincompany component

--- src/components/CompanyReviews/MainCompany.jsx
+++ src/components/CompanyReviews/MainCompany.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa';
-
-
-const BASE_BTN = "relative w-full flex items-center justify-center gap-5 py-2 text-black border border-[#d4d2d0] rounded-lg font-bold  text-[12px] md:text-base transition-all duration-200 ease-in-out cursor-pointer"
+import Link from 'next/link';
+import Populer from './Populer';
 
 export default function MainCompany() {
   return (
-    <div className='min-h-screen flex  items-center justify-center'>
-      <div className='w-full bg-amber-200 p-8'>
-      <div className='space-y-3'>
-          <h1 className='text-black font-bold text-[1.75rem]'>Find great places to work</h1>
-        <p className='text-[#F3F2F1] text-[12px]'>Get access to millions of company reviews</p>
-        <div className='space-y-5'>
-            <p>Company name or job title</p>
-            <div className='w-full flex flex-col md:flex md:flex-row gap-3 '>
-                <div className={BASE_BTN}> <FaSearch className="absolute right-4 w-6 h-6 " /></div>
-                <div className='w-full md:w-1/3 bg-[#3F73D3] p-5 text-center font-bold text-white rounded-lg'>Find companies</div>
+    <div className='min-h-screen flex justify-center items-start bg-white'>
+      <div className="p-8 w-full max-w-3xl">
+        <div className='space-y-3'>
+          <h1 className='text-[#2d2d2d] font-bold text-[1.75rem]'>Find great places to work <span className='text-[#595959] text-[14px] block'> Get access to millions of company reviews</span></h1>
+          <div className='space-y-5'>
+            <p className='text-[#2d2d2d] text-base'>Company name or job title</p>
+            {/* Search-Input */}
+            <div className='w-full flex flex-col md:flex-row gap-3'>
+              <div className="relative w-full  flex items-center justify-center border rounded-md hover:border-b-4 border-[#003a9b] ">
+                <input
+                  type='text'
+                  placeholder='Search...'
+                  className='left-4 w-full pl-2 py-2 outline-none'
+                />
+                <FaSearch className="absolute right-3   text-gray-500 w-5 h-5" />
+              </div>
+              {/* Search-button */}
+              <button className='w-full md:w-1/2 bg-[#003a9b] p-2 text-center items-center text-base font-bold text-white rounded-md '>Find companies</button>
             </div>
+            <Link href="#" className='text-base text-[#003a9b] border-b'>Do you want to search for salaries</Link>
+          </div>
+          <Populer />
         </div>
-      </div>
       </div>
     </div>
   )
